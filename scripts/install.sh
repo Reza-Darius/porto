488711506bda1322c7a6331cde7daf6e614bce50
Darius <Reza.Blersch@gmail.com>
2026-06-25 10:05:55 +0200

    install/uninstall scripts working

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,18 +19,21 @@ BIN_NAME="porto"
 INSTALL_PATH="/usr/local/bin/${BIN_NAME}"
 SERVICE_PATH="/etc/systemd/system/${BIN_NAME}.service"
 
+TMP_FOLDER="/tmp/porto"
+mkdir -p $TMP_FOLDER
+
 echo "downloading binaries"
 
 # download the binary from the repo and make sure the hash matches before installing
 BINARY_URL="https://github.com/Reza-Darius/porto/releases/download/test-release/${BIN_NAME}"
 CHECKSUM_URL="${BINARY_URL}.sha256"
 
-if ! curl -fsSL "$BINARY_URL" -o /tmp/porto; then
+if ! curl -fsSL "$BINARY_URL" -o "${TMP_FOLDER}/porto"; then
   echo "binary download failed"
   exit 1
 fi
 
-if ! curl -fsSL "$CHECKSUM_URL" -o /tmp/porto.sha256; then
+if ! curl -fsSL "$CHECKSUM_URL" -o "${TMP_FOLDER}/porto.sha256"; then
   echo "checksum download failed"
   exit 1
 fi
@@ -43,7 +46,7 @@ fi
 echo "binary passed"
 echo "installing..."
 
-install -o root -g root -m 755 /tmp/porto "${INSTALL_PATH}"
+install -o root -g root -m 755 "${TMP_FOLDER}/porto" "${INSTALL_PATH}"
 
 # create the group if it doesn't exist
 if ! getent group porto >/dev/null 2>&1; then
@@ -65,12 +68,20 @@ fi
 
 # add the install user to the group if not already a member
 if ! id -nG "${INSTALL_USER}" | grep -qw "porto"; then
+  echo "adding $INSTALL_USER to porto group"
   usermod -aG porto "${INSTALL_USER}"
 fi
 
 # generate/install service file
-install -o root -g root -m 644 /tmp/porto.service "${SERVICE_PATH}"
+if ! curl -fsSL "https://raw.githubusercontent.com/Reza-Darius/porto/refs/heads/feat/installer/scripts/porto.service" -o "${TMP_FOLDER}/porto.service"; then
+  echo "failed to download service file"
+  exit 1
+fi
+
+install -o root -g root -m 644 "${TMP_FOLDER}/porto.service" "${SERVICE_PATH}"
 systemctl daemon-reload
 
 # finish up
-su - "{$INSTALL_USER}"
+# su - "${INSTALL_USER}"
+rm -rf $TMP_FOLDER
+echo "install successful!"
